fc08a46f77c7773b7512b26c02afaf1f69ffef56
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-04-11 15:30:12 +0200

    Update license header

--- src/commands/Convert.hpp
+++ src/commands/Convert.hpp
@@ -1,9 +1,5 @@
 // cfiles, an analysis frontend for the Chemfiles library
-// Copyright (C) 2015-2016 Guillaume Fraux
-//
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/
+// Copyright (C) Guillaume Fraux and contributors -- BSD license
 
 #ifndef CFILES_CONVERT_HPP
 #define CFILES_CONVERT_HPP

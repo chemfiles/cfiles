455b21db32cd428f630e4e38edc0f2c251432f9c
Laura Scalfi <laura.scalfi@ens.fr>
2017-04-11 15:00:10 +0200

    Testing some of the HB

--- tests/hbonds.py
+++ tests/hbonds.py
@@ -5,16 +5,26 @@ import os
 TRAJECTORY = os.path.join(os.path.dirname(__file__), "data", "water.xyz")
 OUTPUT = "tmp.dat"
 
+HBONDS = [(39, 0, 1), (270, 0, 2), (69, 3, 4), (75, 3, 5), (228, 6, 8), (66, 9, 10), (3, 9, 11), (24, 18, 19), (27, 18, 20), (42, 21, 22)]
 
 def read_data(path):
     data = []
+    indexes = []
+    frame = True
     with open(path) as fd:
         for line in fd:
             if line.startswith("#"):
+                if line.startswith("# Frame: 1"):
+                    frame = False
                 continue
             dist, angle = map(float, line.split()[-2:])
             data.append((dist, angle))
-    return data
+            if (frame): 
+                donor = int(line.split()[1])
+                acceptor = int(line.split()[3])
+                hydrogen = int(line.split()[5])
+                indexes.append((donor, acceptor, hydrogen))
+    return data, indexes
 
 
 def check_angles(data):
@@ -25,6 +35,10 @@ def check_angles(data):
     assert(max_value[1] <= 30)
     assert(max_value[1] >= 0)
 
+def check_hbonds(indexes):
+    #Check the first hbonds 
+    for bond in HBONDS:
+        assert(bond in indexes)
 
 def hbonds():
     out, err = cfiles(
@@ -36,8 +50,9 @@ def hbonds():
     assert(out == "")
     assert(err == "")
 
-    data = read_data(OUTPUT)
+    data, indexes = read_data(OUTPUT)
     check_angles(data)
+    check_hbonds(indexes)
 
 
 if __name__ == '__main__':

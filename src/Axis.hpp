f47ede4a2f8c5220794e5cf38ce00592cebc073d
Laura Scalfi <laura.scalfi@ens.fr>
2017-04-26 16:50:39 +0200

    Introducing user defined origin

--- src/Axis.hpp
+++ src/Axis.hpp
@@ -26,10 +26,14 @@ public:
         }
     }
     /// Constructor with a 3D vector
-    Axis(double a, double b, double c): vector_(vector3d(a,b,c)/norm(vector3d(a,b,c))) {}
+    Axis(double a, double b, double c): vector_(vector3d(a,b,c)) {
+        /// normalize the axis
+        vector_ = vector_ / norm(vector_);
+    }
 
     /// print axis
-    Vector3D& coordinates() { return vector_; }
+    Vector3D& get_coordinates() { return vector_; }
+    bool is_null() { return vector_ == vector3d(0,0,0); }
 
     /// projection on axis (may be negative)
     double projection(const Vector3D & positions) {

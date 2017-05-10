0f852e03da7d7728ffe8bb576a6b05909d240832
Laura Scalfi <laura.scalfi@ens.fr>
2017-05-10 15:55:05 +0200

    Easy changes requested

--- src/Histogram.hpp
+++ src/Histogram.hpp
@@ -23,14 +23,15 @@ public:
     /// Default constructor
     Histogram(): Histogram(0, 0, 0, 0, 0, 0) {}
     /// Constructor for a flat 2d histogram with a specific number of bins in each direction 
-    /// `n_x` and `n_y`, and which can hold data in the `min_x - max_x` range (resp `min_y - max_y`).
-    Histogram(size_t n_x, double min_x, double max_x, size_t n_y, double min_y, double max_y): super(n_x*n_y) {
-        nx_ = n_x;
-        min_x_ = min_x;
-        dx_ = (max_x - min_x) / n_x;
-        ny_ = n_y;
-        min_y_ = min_y;
-        dy_ = (max_y - min_y) / n_y;
+    /// `nx` and `ny`, and which can hold data in the `min_x - max_x` range (resp `min_y - max_y`).
+    Histogram(size_t nx, double min_x, double max_x, size_t ny, double min_y, double max_y): 
+        super(nx*ny),
+        nx_(nx),
+        min_x_(min_x),
+        dx_((max_x - min_x) / nx),
+        ny_(ny),
+        min_y_(min_y),
+        dy_((max_y - min_y) / ny) {
         static_assert(
             std::is_arithmetic<T>::value,
             "Histogram<T> is only defined for arithmetics types T"
@@ -45,21 +46,25 @@ public:
     Histogram(Histogram&&) = default;
     Histogram& operator=(const Histogram&) = default;
     Histogram& operator=(Histogram&&) = default;
+    /// Operator to get the (i,j) element of a 2D histogram
     const T& operator()(size_t i, size_t j) const {
         return (*this)[j + i * ny_];
     }
 
-    /// Get the size of the bins
+    /// Get the size of the bins in the first dimension
     double dx() const {return dx_;}
+    /// Get the size of the bins in the second dimension
     double dy() const {return dy_;}
 
-    /// Get the minimal value of this histogram
+    /// Get the minimal value of this histogram in the first dimension
     double min_x() const {return min_x_;}
+    /// Get the minimal value of this histogram in the second dimension
     double min_y() const {return min_y_;}
 
-    /// Get the number of bins
-    double n_x() const {return nx_;}
-    double n_y() const {return ny_;}
+    /// Get the number of bins in the first dimension
+    double nx() const {return nx_;}
+    /// Get the number of bins in the second dimension
+    double ny() const {return ny_;}
 
     /// Insert some `x,y` in the histogram
     void insert(T x, T y = 0) {
@@ -85,12 +90,17 @@ public:
         }
     }
 private:
-    /// Width of a bin in the Histogram, for arithmetic types only
+    /// Number of bins in the first dimension
     size_t nx_ = 0;
+    /// Width of a bin in the first dimension
     double dx_ = 0;
+    /// Starting value for the histogram in the first dimension
     double min_x_ = 0;
+    /// Number of bins in the second dimension
     size_t ny_ = 0;
+    /// Width of a bin in the second dimension
     double dy_ = 0;
+    /// Starting value for the histogram in the second dimension
     double min_y_ = 0;
 };
 
